8ca9c64325ed7ccb1ee96d9155ed8e138d16beb9
KimCoal <121536823+KimCoal@users.noreply.github.com>
2025-12-30 09:19:21 +0900

    Feat : .

--- src/store/usePrincipalState.js
+++ src/store/usePrincipalState.js
@@ -3,10 +3,18 @@ import { create } from "zustand";
 export const usePrincipalState = create((set, get) => ({
     isLoggedIn: false,
     principal: null,
+    isLoading: true,
     login: (userData) => set({ isLoggedIn: true, principal: userData }),
     logout: () => {
         localStorage.removeItem("AccessToken");
         set({ isLoggedIn: false, principal: null });
         window.location.href = "/auth/signin";
     },
+
+    setLoading: (isLoading) =>
+        set({
+            isLoggedIn: get().isLoggedIn,
+            principal: get().principal,
+            isLoading: isLoading,
+        }),
 }));
